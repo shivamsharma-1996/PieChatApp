986e7ba0bdd02eee09e34095056cd69ac4ddee7a
shivamsharma <shivamsharma654.ss@gmail.com>
2025-07-06 21:09:47 +0530

    feat(offline-queue): implement functionality in NetworkMonitor to observe network and socket status changes.
    - Initiate socket connection if the device is back online
    - Start Processing MessageQueue with queued messages when the connection is established again

--- app/src/main/java/com/shivam/piechatapp/data/network/NetworkMonitor.kt
+++ app/src/main/java/com/shivam/piechatapp/data/network/NetworkMonitor.kt
@@ -1,5 +1,12 @@
 package com.shivam.piechatapp.data.network
 
+import com.shivam.piechatapp.data.handler.MessageHandler
+import com.shivam.piechatapp.data.repository.PieSocketWebSocketRepository
+import com.shivam.piechatapp.domain.model.ConnectionStatus
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -7,15 +14,35 @@ import javax.inject.Singleton
 @Singleton
 class NetworkMonitor @Inject constructor(
     private val networkStatusManager: NetworkStatusManager,
+    private val messageHandler: MessageHandler,
+    private val socketRepository: PieSocketWebSocketRepository
 ) {
 
     fun startMonitoring() {
         networkStatusManager.startNetworkMonitoring()
 
-        // TODO - Observe two things - network status changes 2. socket connection status changes
-    }
+        // Observe network status changes
+        CoroutineScope(Dispatchers.IO).launch {
+            networkStatusManager.networkStatusFlow.collectLatest { isOnline ->
+                if (isOnline) {
+                    // Reconnect PieSocket when back online
+                    socketRepository.connect()
+                }
+            }
+        }
 
-    fun stopMonitoring() {
-        networkStatusManager.stopNetworkMonitoring()
+        // Observe socket connection status changes
+        CoroutineScope(Dispatchers.IO).launch {
+            socketRepository.getConnectionStatus().collectLatest { status ->
+                if (status == ConnectionStatus.Connected && messageHandler.hasQueuedMessages()) {
+                    // Process queued messages when socket connects
+                    messageHandler.processQueuedMessages()
+                }
+            }
+        }
+
+        fun stopMonitoring() {
+            networkStatusManager.stopNetworkMonitoring()
+        }
     }
 }
