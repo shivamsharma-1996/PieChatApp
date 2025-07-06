d1b9a1d647cff16d828ab22e8f4a5fa83f8cfe7a
shivamsharma <shivamsharma654.ss@gmail.com>
2025-07-06 10:08:58 +0530

    fix: bump minsdk to 27 from 24 to min. requirement of PieSocket SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.shivam.piechatapp"
-        minSdk = 24
+        minSdk = 27
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
