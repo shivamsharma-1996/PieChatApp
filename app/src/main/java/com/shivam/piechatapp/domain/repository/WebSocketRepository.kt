a9539aa5c68659aff0b50a20e249f9a3293cb824
shivamsharma <shivamsharma654.ss@gmail.com>
2025-07-06 09:34:10 +0530

    feat: add Conversation and WebSocket repositories

---
+++ app/src/main/java/com/shivam/piechatapp/domain/repository/WebSocketRepository.kt
@@ -0,0 +1,12 @@
+package com.shivam.piechatapp.domain.repository
+
+import com.shivam.piechatapp.domain.model.ChatMessage
+import kotlinx.coroutines.flow.Flow
+
+interface WebSocketRepository {
+    fun getMessages(): Flow<List<ChatMessage>>
+    fun connect()
+    fun disconnect()
+    fun sendMessage(message: ChatMessage): Result<Unit>
+    fun isConnected(): Boolean
+} 
